e3bcea409437a77142fc5e9de418e9e474a3dbd8
Musab Ibnu Siraj <musab.dot@gmail.com>
2024-01-16 08:48:50 +0530

    user create if not exist

--- test.php
+++ test.php
@@ -7,6 +7,14 @@ include __DIR__ . '/helpers/AppManager.php';
 require_once __DIR__ . '/models/User.php';
 
 $userModel = new User();
+
+if ($userModel->createUser("musab", "mypassword123", "operator", "musab@example.com")) {
+    echo "User created successfully!";
+} else {
+    echo "Failed to create user. May be user already exist!";
+}
+
+
 $users = $userModel->getAllActive();
 
 dd($users);
