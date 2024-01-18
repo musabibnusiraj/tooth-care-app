74c08f38e5a6f01fa5c265fd8210c66afffa1fcd
Musab Ibnu Siraj <musab.dot@gmail.com>
2024-01-18 09:51:51 +0530

    create form

--- views/admin/users.php
+++ views/admin/users.php
@@ -9,7 +9,14 @@ $users = $userModel->getAll();
 
 <div class="container">
 
-    <h1 class="mx-3 my-5">Users</h1>
+    <h1 class="mx-3 my-5">
+        Users
+
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary float-end m-3" data-bs-toggle="modal" data-bs-target="#basicModal">
+            Create User
+        </button>
+    </h1>
     <section class="content m-3">
         <div class="container-fluid">
             <div class="card">
@@ -65,34 +72,56 @@ $users = $userModel->getAll();
     </section>
 </div>
 
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#basicModal">
-    Launch modal
-</button>
 
 <!-- Modal -->
 <div class="modal fade" id="basicModal" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel1">Modal title</h5>
+                <h5 class="modal-title" id="exampleModalLabel1">Create User</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <div class="row">
                     <div class="col mb-3">
-                        <label for="nameBasic" class="form-label">Name</label>
-                        <input type="text" id="nameBasic" class="form-control" placeholder="Enter Name" />
+                        <label for="username" class="form-label">User Name</label>
+                        <input type="text" id="username" name="username" class="form-control" placeholder="Enter Name" />
                     </div>
                 </div>
-                <div class="row g-2">
+                <div class="row g-1">
                     <div class="col mb-0">
-                        <label for="emailBasic" class="form-label">Email</label>
-                        <input type="text" id="emailBasic" class="form-control" placeholder="xxxx@xxx.xx" />
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" id="email" name="email" class="form-control" placeholder="xxxx@xxx.xx" />
                     </div>
+
+                </div>
+                <div class="row g-2 mt-2">
+                    <div class="col mb-0 form-password-toggle">
+                        <label class="form-label" for="password">Password</label>
+                        <div class="input-group">
+                            <input type="password" name="password" class="form-control" id="password" placeholder="············" aria-describedby="basic-default-password2">
+                            <span id="basic-default-password2" class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                        </div>
+                    </div>
+                    <div class="col mb-0 form-password-toggle">
+                        <label class="form-label" for="basic-default-password12">Password</label>
+                        <div class="input-group">
+                            <input type="password" class="form-control" id="basic-default-password12" placeholder="············" aria-describedby="basic-default-password2">
+                            <span id="basic-default-password2" class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-3">
                     <div class="col mb-0">
-                        <label for="dobBasic" class="form-label">DOB</label>
-                        <input type="text" id="dobBasic" class="form-control" placeholder="DD / MM / YY" />
+                        <label class="form-label" for="permission">Permission</label>
+                        <div class="input-group">
+                            <label class="input-group-text" for="inputGroupSelect01">Options</label>
+                            <select class="form-select" id="permission" name="permission">
+                                <option selected="">Choose...</option>
+                                <option value="operator">Operator</option>
+                                <option value="doctor">Doctor</option>
+                            </select>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -100,7 +129,7 @@ $users = $userModel->getAll();
                 <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
                     Close
                 </button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-primary">Save</button>
             </div>
         </div>
     </div>
@@ -109,3 +138,11 @@ $users = $userModel->getAll();
 <?php
 require_once('../layouts/footer.php');
 ?>
+
+<script>
+    $(document).ready(function() {
+
+
+
+    });
+</script>
